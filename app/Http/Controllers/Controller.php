49e22f7be9dc88a25355a9b1f0c89451a0b7c0ce
rain <240918304@qq.com>
2023-09-26 19:57:44 +0800

    base控制器添加统一返回格式

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,31 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    /**
+     * 返回成功
+     * @param string|array|null $data
+     * @return array
+     */
+    public function success(null|string|array $data = null): array
+    {
+        return [
+            'code' => 200,
+            'data' => $data
+        ];
+    }
+
+    /**
+     * 返回成功
+     * @param string $message
+     * @param int $code
+     * @return array
+     */
+    public function error(string $message, int $code = 600): array
+    {
+        return [
+            'code' => $code,
+            'message' => $message
+        ];
+    }
 }

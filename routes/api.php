ab68ba02796f91f79cfd1bad984f0b89fba49e50
rain <240918304@qq.com>
2023-09-27 20:46:04 +0800

    完成任务要求的路由

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,44 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/return-request', function (Request $request) {
+    return $request->all();
+});
+
+Route::get('/validate-exception', function (Request $request) {
+    $request->validate(['test' => 'required']);
+});
+
+Route::get('/error', function () {
+    1 / 0;
+});
+
+Route::get('/test', function (Request $request) {
+    $s = $request->s;
+    $length = strlen($s);
+    if ($length <= 1 || $length >= 10000) {
+        return ['code' => 500, 'message' => 'invalidate length'];
+    }
+    $chars = ['(', ')', '[', ']', '{', '}'];
+    $stack = [];
+    $result = true;
+    for ($i = 0; $i < $length; $i++) {
+        $index = array_search($s[$i], $chars);
+        if ($index === false) {
+            return ['code' => 500, 'data' => 'invalidate character'];
+        }
+        if ($index % 2 == 0) {
+            $stack[] = $s[$i];
+        } else {
+            if (array_pop($stack) !== $chars[$index - 1]) {
+                $result = false;
+                break;
+            }
+        }
+    }
+    if (!empty($stack)) {
+        $result = false;
+    }
+    return ['code' => 200, 'data' => $result];
+});
